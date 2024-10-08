6b34a9398f24ac446e08ba1bcc356488030d0721
Chathushka <chathushkahome9@gmail.com>
2024-10-08 16:29:21 +0530

    Update data_loader.py

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -224,9 +224,9 @@ class Dataset_Custom(Dataset):
         # Load data based on set_type (1: train, 2: test, 3: validation)
         if self.set_type == 0:
             df_raw = pd.read_csv(os.path.join(self.root_path, "train.csv"))
-        elif self.set_type == 1:
-            df_raw = pd.read_csv(os.path.join(self.root_path, "test.csv"))
         elif self.set_type == 2:
+            df_raw = pd.read_csv(os.path.join(self.root_path, "test.csv"))
+        elif self.set_type == 1:
             df_raw = pd.read_csv(os.path.join(self.root_path, "val.csv"))
 
         '''
