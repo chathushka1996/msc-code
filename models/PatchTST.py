15e5af6be06ea391bdd66e74cddae83429ced8aa
Chathushka <chathushkahome9@gmail.com>
2024-10-09 09:58:14 +0530

    Update PatchTST.py

--- models/PatchTST.py
+++ models/PatchTST.py
@@ -41,61 +41,44 @@ class Model(nn.Module):
         revin = configs.revin
         affine = configs.affine
         subtract_last = configs.subtract_last
-        
-        decomposition = configs.decomposition
+
         kernel_size = configs.kernel_size
         
         
         # model
-        self.decomposition = decomposition
-        if self.decomposition:
-            self.decomp_module = series_decomp_2(kernel_size)
-            self.model_trend = PatchTST_backbone(c_in=c_in, context_window = context_window, target_window=target_window, patch_len=patch_len, stride=stride, 
-                                  max_seq_len=max_seq_len, n_layers=n_layers, d_model=d_model,
-                                  n_heads=n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm, attn_dropout=attn_dropout,
-                                  dropout=dropout, act=act, key_padding_mask=key_padding_mask, padding_var=padding_var, 
-                                  attn_mask=attn_mask, res_attention=res_attention, pre_norm=pre_norm, store_attn=store_attn,
-                                  pe=pe, learn_pe=learn_pe, fc_dropout=fc_dropout, head_dropout=head_dropout, padding_patch = padding_patch,
-                                  pretrain_head=pretrain_head, head_type=head_type, individual=individual, revin=revin, affine=affine,
-                                  subtract_last=subtract_last, verbose=verbose, **kwargs)
-            self.model_seasonal = PatchTST_backbone(c_in=c_in, context_window = context_window, target_window=target_window, patch_len=patch_len, stride=stride, 
-                                  max_seq_len=max_seq_len, n_layers=n_layers, d_model=d_model,
-                                  n_heads=n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm, attn_dropout=attn_dropout,
-                                  dropout=dropout, act=act, key_padding_mask=key_padding_mask, padding_var=padding_var, 
-                                  attn_mask=attn_mask, res_attention=res_attention, pre_norm=pre_norm, store_attn=store_attn,
-                                  pe=pe, learn_pe=learn_pe, fc_dropout=fc_dropout, head_dropout=head_dropout, padding_patch = padding_patch,
-                                  pretrain_head=pretrain_head, head_type=head_type, individual=individual, revin=revin, affine=affine,
-                                  subtract_last=subtract_last, verbose=verbose, **kwargs)
-            self.model_residual = PatchTST_backbone(c_in=c_in, context_window = context_window, target_window=target_window, patch_len=patch_len, stride=stride, 
-                                  max_seq_len=max_seq_len, n_layers=n_layers, d_model=d_model,
-                                  n_heads=n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm, attn_dropout=attn_dropout,
-                                  dropout=dropout, act=act, key_padding_mask=key_padding_mask, padding_var=padding_var, 
-                                  attn_mask=attn_mask, res_attention=res_attention, pre_norm=pre_norm, store_attn=store_attn,
-                                  pe=pe, learn_pe=learn_pe, fc_dropout=fc_dropout, head_dropout=head_dropout, padding_patch = padding_patch,
-                                  pretrain_head=pretrain_head, head_type=head_type, individual=individual, revin=revin, affine=affine,
-                                  subtract_last=subtract_last, verbose=verbose, **kwargs)
-        else:
-            self.model = PatchTST_backbone(c_in=c_in, context_window = context_window, target_window=target_window, patch_len=patch_len, stride=stride, 
-                                  max_seq_len=max_seq_len, n_layers=n_layers, d_model=d_model,
-                                  n_heads=n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm, attn_dropout=attn_dropout,
-                                  dropout=dropout, act=act, key_padding_mask=key_padding_mask, padding_var=padding_var, 
-                                  attn_mask=attn_mask, res_attention=res_attention, pre_norm=pre_norm, store_attn=store_attn,
-                                  pe=pe, learn_pe=learn_pe, fc_dropout=fc_dropout, head_dropout=head_dropout, padding_patch = padding_patch,
-                                  pretrain_head=pretrain_head, head_type=head_type, individual=individual, revin=revin, affine=affine,
-                                  subtract_last=subtract_last, verbose=verbose, **kwargs)
+        self.decomp_module = series_decomp_2(kernel_size)
+        self.model_trend = PatchTST_backbone(c_in=c_in, context_window = context_window, target_window=target_window, patch_len=patch_len, stride=stride, 
+                                max_seq_len=max_seq_len, n_layers=n_layers, d_model=d_model,
+                                n_heads=n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm, attn_dropout=attn_dropout,
+                                dropout=dropout, act=act, key_padding_mask=key_padding_mask, padding_var=padding_var, 
+                                attn_mask=attn_mask, res_attention=res_attention, pre_norm=pre_norm, store_attn=store_attn,
+                                pe=pe, learn_pe=learn_pe, fc_dropout=fc_dropout, head_dropout=head_dropout, padding_patch = padding_patch,
+                                pretrain_head=pretrain_head, head_type=head_type, individual=individual, revin=revin, affine=affine,
+                                subtract_last=subtract_last, verbose=verbose, **kwargs)
+        self.model_seasonal = PatchTST_backbone(c_in=c_in, context_window = context_window, target_window=target_window, patch_len=patch_len, stride=stride, 
+                                max_seq_len=max_seq_len, n_layers=n_layers, d_model=d_model,
+                                n_heads=n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm, attn_dropout=attn_dropout,
+                                dropout=dropout, act=act, key_padding_mask=key_padding_mask, padding_var=padding_var, 
+                                attn_mask=attn_mask, res_attention=res_attention, pre_norm=pre_norm, store_attn=store_attn,
+                                pe=pe, learn_pe=learn_pe, fc_dropout=fc_dropout, head_dropout=head_dropout, padding_patch = padding_patch,
+                                pretrain_head=pretrain_head, head_type=head_type, individual=individual, revin=revin, affine=affine,
+                                subtract_last=subtract_last, verbose=verbose, **kwargs)
+        self.model_residual = PatchTST_backbone(c_in=c_in, context_window = context_window, target_window=target_window, patch_len=patch_len, stride=stride, 
+                                max_seq_len=max_seq_len, n_layers=n_layers, d_model=d_model,
+                                n_heads=n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm, attn_dropout=attn_dropout,
+                                dropout=dropout, act=act, key_padding_mask=key_padding_mask, padding_var=padding_var, 
+                                attn_mask=attn_mask, res_attention=res_attention, pre_norm=pre_norm, store_attn=store_attn,
+                                pe=pe, learn_pe=learn_pe, fc_dropout=fc_dropout, head_dropout=head_dropout, padding_patch = padding_patch,
+                                pretrain_head=pretrain_head, head_type=head_type, individual=individual, revin=revin, affine=affine,
+                                subtract_last=subtract_last, verbose=verbose, **kwargs)
     
     
     def forward(self, x):           # x: [Batch, Input length, Channel]
-        if self.decomposition:
-            trend_init, seasonal_init, residual_init = self.decomp_module(x)
-            trend_init, seasonal_init, residual_init = trend_init.permute(0, 2, 1), seasonal_init.permute(0, 2, 1), residual_init.permute(0, 2, 1)  # x: [Batch, Channel, Input length]
-            trend = self.model_trend(trend_init)
-            seasonal = self.model_seasonal(seasonal_init)
-            residual = self.model_residual(residual_init)
-            x = trend + seasonal + residual
-            x = x.permute(0,2,1)    # x: [Batch, Input length, Channel]
-        else:
-            x = x.permute(0,2,1)    # x: [Batch, Channel, Input length]
-            x = self.model(x)
-            x = x.permute(0,2,1)    # x: [Batch, Input length, Channel]
+        trend_init, seasonal_init, residual_init = self.decomp_module(x)
+        trend_init, seasonal_init, residual_init = trend_init.permute(0, 2, 1), seasonal_init.permute(0, 2, 1), residual_init.permute(0, 2, 1)  # x: [Batch, Channel, Input length]
+        trend = self.model_trend(trend_init)
+        seasonal = self.model_seasonal(seasonal_init)
+        residual = self.model_residual(residual_init)
+        x = trend + seasonal + residual
+        x = x.permute(0,2,1)    # x: [Batch, Input length, Channel]
         return x
