aabd30392d670f6e7b9a95dcb6f70a2782fdd6f9
Chathushka <chathushkahome9@gmail.com>
2025-02-22 18:33:00 +0530

    ch

--- exp/exp_main.py
+++ exp/exp_main.py
@@ -1,6 +1,6 @@
 from data_provider.data_factory import data_provider
 from exp.exp_basic import Exp_Basic
-from models import AutoPatchTST, CNNpatchTST, DecoPatchTST, DecompPatchTST, DecomposedInformer, FourierPatchTST, HybridPatchTST_DLinear, DLinear, HybridPatchTST_DLinear_A, HybridPatchTST_DLinear_W, ImprovedPatchTST, Informer, PatchInformer, PatchTST, RNNPatchTST
+from models import AutoPatchTST, DecoPatchTST, DecompPatchTST, DecomposedInformer, FourierPatchTST, HybridPatchTST, HybridPatchTST_DLinear, DLinear, HybridPatchTST_DLinear_A, HybridPatchTST_DLinear_W, ImprovedPatchTST, Informer, PatchInformer, PatchTST, RNNPatchTST
 from utils.tools import EarlyStopping, adjust_learning_rate, visual, test_params_flop
 from utils.metrics import metric
 
@@ -39,7 +39,7 @@ class Exp_Main(Exp_Basic):
             'PatchInformer': PatchInformer,
             'Informer': Informer,
             'DecomposedInformer': DecomposedInformer,
-            'CNNpatchTST': CNNpatchTST
+            'HybridPatchTST': HybridPatchTST
         }
         model = model_dict[self.args.model].Model(self.args).float()
 
--- patchtst_dlinear.sh
+++ patchtst_dlinear.sh
@@ -5,7 +5,7 @@ if [ ! -d $path/logs ]; then
 fi
 
 seq_len=96
-model_name=CNNpatchTST
+model_name=HybridPatchTST
 dataset=sl
 root_path_name=./data/$dataset
 data_path_name=solar.csv
